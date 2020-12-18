b619664f4ca3f471f8f2752d6032d84a9a58d0aa
Panda <849010374@qq.com>
2020-12-18 14:58:53 +0000

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ln -sfv $PWD/source/.vimrc ~/.vimrc && cp -av ./source/autoload/ ./source/colors/ ~/.vim
+if ln -sfv $PWD/source/.vimrc ~/.vimrc && cp -av ./source/autoload/ ./source/colors/ ~/.vim/
 then
     echo "Install Successful !!!"
 else
