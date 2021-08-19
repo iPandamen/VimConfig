1158f086b6e589bfcd5747d6082fe5b977320b1c
xiong <feixiong.zhou@brstar.com.cn>
2021-08-19 17:32:13 +0800

    Update Windows install script

--- Window_install.bat
+++ Window_install.bat
@@ -1,4 +1,4 @@
-copy .vimrc C:\Users\%USERNAME%\.vimrc
-xcopy autoload C:\Users\%USERNAME%\.vim\autoload\ /E
-xcopy colors C:\Users\%USERNAME%\.vim\colors\ /E
-echo Successfully
+copy source\.vimrc %HOMEDRIVE%%HOMEPATH% &&  echo "'copy source\.vimrc %HOMEDRIVE%%HOMEPATH%' execute successfully"
+xcopy source\plugin %HOMEDRIVE%%HOMEPATH%\.vim\plugin /e /i && echo "'xcopy source\plugin %HOMEDRIVE%%HOMEPATH%\.vim\plugin /e /i '  executesuccessfully"
+xcopy source\autoload %HOMEDRIVE%%HOMEPATH%\.vim\autoload /e /i && echo "'xcopy source\autoload %HOMEDRIVE%%HOMEPATH%\.vim\autoload /e /i '  executesuccessfully"
+xcopy source\colors %HOMEDRIVE%%HOMEPATH%\.vim\colors /e /i && echo "'xcopy source\colors %HOMEDRIVE%%HOMEPATH%\.vim\colors /e /i' execute successfully"
